ce8a7831f3ad1484a526618d5a16f9c6fdd68f9e
Nicolas Graves <ngraves@ngraves.fr>
2025-05-07 13:02:58 +0200

    Add some context for running it

---
+++ .guix/modules/guile-dmenu-package.scm
@@ -0,0 +1,51 @@
+(define-module (guile-dmenu-package)
+  #:use-module (ice-9 match)
+  #:use-module (guix packages)
+  #:use-module (guix profiles)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages ibus)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages file)
+  #:use-module (gnu packages build-tools)
+  #:use-module (guile-wayland packages guile-wayland)
+  #:use-module (guile-wayland packages guile-xyz))
+
+(define-public guile-bytestructure-class-next
+  ((package-input-rewriting/spec
+    `(("guile" . ,(const guile-next))))
+   (package/inherit guile-bytestructure-class
+     (arguments
+      ;; XXX: guile-next breaks one test.
+      (list #:tests? #f
+            #:make-flags #~'("GUILE_AUTO_COMPILE=0"))))))
+
+(define next
+  (package-input-rewriting/spec
+   `(("guile" . ,(const guile-next))
+     ("guile-bytestructure-class" . ,(const guile-bytestructure-class-next))
+     ;; Don't try to rebuild those.
+     ("swig" . ,(const (@ (gnu packages swig) swig)))
+     ("graphviz" . ,(const (@ (gnu packages graphviz) graphviz))))))
+
+(match (cadr (command-line))
+  ("build" guile-wayland)
+  ("shell" (packages->manifest
+            (append (map next
+                         (list guile-cairo
+                               guile-wayland
+                               guile-xkbcommon))
+                    (list guile-next
+                          guile-ares-rs
+                          guile-bytestructure-class-next))))
+  (_ (package->development-manifest guile-wayland)))
